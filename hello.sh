8443e3fd388afcc0570d5329631df66d8a702353
Hamza <hamzaredone6@gmail.com>
2024-10-05 12:38:22 +0000

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,14 +1,119 @@
-echo "Never gonna give you up"
-echo "Never gonna let you down"
-echo "Never gonna run around and desert you"
-echo "Never gonna make you cry"
-echo "Never gonna say goodbye"
-echo "Never gonna tell a lie and hurt you"
-echo "Never gonna stop"
-echo "Never gonna give you up"
-echo "Never gonna let you down"
-echo "Never gonna run around and desert you"
-echo "Never gonna make you cry"
-echo "Never gonna say goodbye"
-echo "Never gonna tell a lie and hurt you"
-echo "//////////Dont run random shit from the internet kids//////////"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+ 
+
+
+echo -e "${GREEN}\"We're no strangers to love\"${NC}"
+echo -e "${GREEN}\"You know the rules and so do I (do I)\"${NC}"
+echo -e "${GREEN}\"A full commitment's what I'm thinking of\"${NC}"
+echo -e "${GREEN}\"You wouldn't get this from any other guy\"${NC}"
+echo -e "${GREEN}\"I just wanna tell you how I'm feeling\"${NC}"
+echo -e "${GREEN}\"Gotta make you understand\"${NC}"
+echo -e "${GREEN}\"Never gonna give you up\"${NC}"
+echo -e "${PURPLE}\"Never gonna let you down\"${NC}"
+echo -e "${PURPLE}\"Never gonna run around and desert you\"${NC}"
+echo -e "${PURPLE}\"Never gonna make you cry\"${NC}"
+echo -e "${PURPLE}\"Never gonna say goodbye\"${NC}"
+echo -e "${PURPLE}\"Never gonna tell a lie and hurt you\"${NC}"
+echo -e "${PURPLE}\"We've known each other for so long\"${NC}"
+echo -e "${PURPLE}\"Your heart's been aching, but you're too shy to say it (say it)\"${NC}"
+echo -e "${PURPLE}\"Inside, we both know what's been going on (going on)\"${NC}"
+echo -e "${PURPLE}\"We know the game and we're gonna play it\"${NC}"
+echo -e "${PURPLE}\"And if you ask me how I'm feeling\"${NC}"
+echo -e "${PURPLE}\"Don't tell me you're too blind to see\"${NC}"
+echo -e "${PURPLE}\"Never gonna give you up\"${NC}"
+echo -e "${PURPLE}\"Never gonna let you down\"${NC}"
+echo -e "${PURPLE}\"Never gonna run around and desert you\"${NC}"
+echo -e "${PURPLE}\"Never gonna make you cry\"${NC}"
+echo -e "${PURPLE}\"Never gonna say goodbye\"${NC}"
+echo -e "${PURPLE}\"Never gonna tell a lie and hurt you\"${NC}"
+echo -e "${PURPLE}\"Never gonna give you up\"${NC}"
+echo -e "${PURPLE}\"Never gonna let you down\"${NC}"
+echo -e "${PURPLE}\"Never gonna run around and desert you\"${NC}"
+echo -e "${PURPLE}\"Never gonna make you cry\"${NC}"
+echo -e "${PURPLE}\"Never gonna say goodbye\"${NC}"
+echo -e "${PURPLE}\"Never gonna tell a lie and hurt you\"${NC}"
+echo -e "${PURPLE}\"We've known each other for so long\"${NC}"
+echo -e "${PURPLE}\"Your heart's been aching, but you're too shy to say it (to say it)\"${NC}"
+echo -e "${PURPLE}\"Inside, we both know what's been going on (going on)\"${NC}"
+echo -e "${PURPLE}\"We know the game and we're gonna play it\"${NC}"
+echo -e "${PURPLE}\"I just wanna tell you how I'm feeling\"${NC}"
+echo -e "${BLUE}\"Gotta make you understand\"${NC}"
+echo -e "${BLUE}\"Never gonna give you up\"${NC}"
+echo -e "${BLUE}\"Never gonna let you down\"${NC}"
+echo -e "${BLUE}\"Never gonna run around and desert you\"${NC}"
+echo -e "${BLUE}\"Never gonna make you cry\"${NC}"
+echo -e "${BLUE}\"Never gonna say goodbye\"${NC}"
+echo -e "${CYAN}\"Never gonna tell a lie and hurt you\"${NC}"
+echo -e "${CYAN}\"Never gonna give you up\"${NC}"
+echo -e "${CYAN}\"Never gonna let you down\"${NC}"
+echo -e "${CYAN}\"Never gonna run around and desert you\"${NC}"
+echo -e "${CYAN}\"Never gonna make you cry\"${NC}"
+echo -e "${CYAN}\"Never gonna say goodbye\"${NC}"
+echo -e "${CYAN}\"Never gonna tell a lie and hurt you\"${NC}"
+echo -e "${CYAN}\"Never gonna give you up\"${NC}"
+echo -e "${CYAN}\"Never gonna let you down\"${NC}"
+echo -e "${CYAN}\"Never gonna run around and desert you\"${NC}"
+echo -e "${CYAN}\"Never gonna make you cry\"${NC}"
+echo -e "${CYAN}\"Never gonna say goodbye\"${NC}"
+echo -e "${CYAN}\"Never gonna tell a lie and hurt you\"${NC}"
+echo -e "${CYAN}\"Never gonna give you up\"${NC}"
+echo -e "${CYAN}\"Never gonna let you down\"${NC}"
+echo -e "${CYAN}\"Never gonna run around and desert you\"${NC}"
+echo -e "${CYAN}\"Never gonna make you cry\"${NC}"
+echo -e "${CYAN}\"Never gonna say goodbye\"${NC}"
+echo -e "${CYAN}\"Never gonna tell a lie and hurt you\"${NC}"
+echo -e "${CYAN}\"Never gonna give you up\"${NC}"
+echo -e "${CYAN}\"Never gonna let you down\"${NC}"
+
+ 
+ 
+ 
+ echo -e "${RED}
+
+░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░▒▓████████▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓██████████████▓▒░             
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░            
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░            
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░            
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░            
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░            
+░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░    
+
+ 
+
+ ░▒▓███████▓▒░░▒▓██████▓▒░░▒▓███████▓▒░░▒▓█▓▒░▒▓███████▓▒░▒▓████████▓▒░▒▓███████▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓█▓▒░        
+░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓█▓▒░        
+ ░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░▒▓███████▓▒░  ░▒▓█▓▒░   ░▒▓██████▓▒░            
+       ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▒░         ░▒▓█▓▒░ 
+       ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▒░         ░▒▓█▓▒░ 
+░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▒░  ░▒▓███████▓▒░   
+
+
+                                                                                    
+░▒▓████████▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓██████████████▓▒░       ░▒▓█▓▒░▒▓███████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓███████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓████████▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     
+░▒▓██████▓▒░ ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓██████▓▒░ ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░    ░▒▓█▓▒░     
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░  ░▒▓█▓▒░     
+${NC}"
+
+
+
+
+echo -e "${RED}
+
+
+
+YOU HAVE BEEN RICKROLLED BY ME !
+
+
+
+${NC}"
